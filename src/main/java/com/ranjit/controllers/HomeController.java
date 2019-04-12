26a1e7cc16eb432ddcbc47be8772759adcc5cac8
theranjitkumar <ranjit.kumar@mobileprogramming.net>
2019-04-12 21:09:09 +0530

    login logout button added in about page

--- src/main/java/com/ranjit/controllers/HomeController.java
+++ src/main/java/com/ranjit/controllers/HomeController.java
@@ -1,30 +1,49 @@
 package com.ranjit.controllers;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-//	@GetMapping({"/"})
-//	public String home() {
-//		return "post";
-//	}
+	String username;
+
+	// @GetMapping({"/"})
+	// public String home() {
+	// return "post";
+	// }
 
 	@GetMapping("/about")
-	public String about() {
-		return "about";
+	public ModelAndView about(ModelAndView mv, Principal principal) {
+
+		try {
+
+			username = principal.getName();
+			System.out.println(username);
+
+		} catch (NullPointerException e) {
+			System.out.print("NullPointerException Caught");
+		}
+		if(username !=null){
+			mv.addObject("username", username);
+		}
+		mv.setViewName("about");
+		return mv;
 	}
 
 	@GetMapping("/contactus")
 	public String contactus() {
 		return "contactus";
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
+
 	@GetMapping("/logout")
 	public String logout() {
 		return "login";
